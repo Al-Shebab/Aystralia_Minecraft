477f53ef1044f1a559ed157556acb1ee7665bd14
Al Shebab <70445073+alshebabicus@users.noreply.github.com>
2021-03-13 19:48:03 -0800

    add server backward compatibility

--- hub/start.bat
+++ hub/start.bat
@@ -1,24 +1,16 @@
 @echo off
-title Hub Server
+title Hub 1 Server
 color 0A
 prompt [server]:
 cls
 
 :start
 echo loading server...
-java -Xmx2G -jar paper.jar --nogui
+java -Xmx1G -jar paper.jar --nogui
 cls
 
 :restart
-cls
-echo server will restart
-TIMEOUT /T 5
-cls
 goto :start
 
 :stop
-
-cls
-echo closing server
-TIMEOUT /T 5
 exit
